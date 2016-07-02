f91a3731835f4e74384bc48900097d8a2b5b473d
Elva <799584377@qq.com>
2016-07-02 13:00:46 +0800

    test3

---
+++ js/info.js
@@ -0,0 +1,39 @@
+var info=[[{
+	   name:"Uelike(优艺客)",
+	   des:"还原Uelike官网",
+	   detail:["官网首页的整体制作，包括HTML和所有JS动态效果；","项目开发运用到CSS3、jquery、Ajax，DOM操作、BOM操作，还有自适应，实现了轮播图，拖拽窗口，自定义滚动条，选项卡等页面上常用功能；","做了性能优化，例如：尽量作用JSON格式来创建对象；替换、查找，就使用正则表达式，提高性能"],
+	   plink:"link/pc/uelike/uelike.html"
+},{
+		name:"爱奇艺(pc)",
+		des:"仿照爱奇艺做静态页面",
+		detail:["做到全浏览器兼容（示例效果，布局做出来啦，具体内容没填充）"],
+		plink:"link/pc/aiqiyi-pc/index.html"
+	}
+	],[{
+		name:"回家gohome",
+		des:" 项目利用的移动端插件swiper，让代码写起来更简单，效果简洁流畅",
+		plink:"link/mob/gohome/index.html"
+	},{
+		name:"定时器",
+		des:"手机设置定时器倒计时",
+		detail:["项目主要利用css3的transform 旋转，再加上巧妙布局（该处与十六宫格的核心部分布局一样，利用div span多层嵌套的，给一类标签添加样式）"],plink:"link/mob/countdown/index.html"
+	},{
+		name:"手机滑动切换效果",
+		des:"仿锤子手机十六宫格主题切换效果",
+		detail:["界面主要利用CSS3,进行了十六宫格布局，每个格子都是一个3D立体的盒子，每一面为要显示的图片","滑动切换时，给每个盒子相应旋转沿Y轴旋转正负90","为达到渐次翻转的效果，添加定时器，延时翻转","添加回弹动画效果"],
+		plink:"link/mob/phone.html"
+	}
+	],[{
+		name:"拼图游戏",
+		des:"简单的拼图小游戏",
+		detail:[
+		"游戏设置很简单，可以选图片，选级别：简单、一般、困难，可提示，可一键还原","拖动图片换位置，通过难度升级"],plink:"link/game/puzzle/pintu.html"
+	},{
+			name:"贪吃蛇",
+			des:"简单的贪吃蛇小游戏，时间有点久远，bug挺多，拿来给大家吐槽下好啦~",
+			plink:"link/game/Snake/snake.html"
+		}]
+]
+	
+	
+
